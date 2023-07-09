13c255d265fcdfc5e73935c27f2683a32857e373
ANDERSON DA SILVA FERNANDES <64322982+Anderson2022@users.noreply.github.com>
2023-07-08 21:14:08 -0300

    "feat: add quickSeach components"

--- src/app/components/TripSearch.tsx
+++ src/app/components/TripSearch.tsx
@@ -1,24 +1,82 @@
-import { ComponentPropsWithoutRef, LegacyRef, forwardRef } from "react";
-import { twMerge } from "tailwind-merge";
+"use client";
 
-interface InputProps extends ComponentPropsWithoutRef<"input"> {
-  error?: boolean;
-  errorMessage?: string;
+
+import DatePicker from "@/components/DatePicker";
+import Input from "@/components/Input";
+import { useRouter } from "next/navigation";
+import React from "react";
+import Button from "@/components/Button";
+import { Controller, useForm } from "react-hook-form";
+
+interface TripSearchForm {
+  text: string;
+  startDate: Date | null;
+  budget: string;
 }
 
-function Input({ className, error, errorMessage, ...props }: InputProps, ref: LegacyRef<HTMLInputElement> | undefined) {
-  const inputClassName = twMerge(
-    "rounded-lg border border-gray-300 bg-white p-2 text-sm font-normal text-primaryDarker placeholder-black placeholder-opacity-20 outline-none transition-all",
-    error ? "border-red-500" : "focus:ring-1 focus:ring-primary",
-    className
-  );
+const TripSearch = () => {
+  const router = useRouter();
+
+  const {
+    control,
+    formState: { errors },
+    register,
+    handleSubmit,
+  } = useForm<TripSearchForm>();
+
+  const onSubmit = (data: TripSearchForm) => {
+    router.push(`/trips/search?text=${data.text}&startDate=${data.startDate?.toISOString()}&budget=${data.budget}`);
+  };
 
   return (
-    <div className="flex w-full flex-col">
-      <input ref={ref} className={inputClassName} {...props} />
-      {error && errorMessage && <span className="mt-1 text-xs text-red-400">{errorMessage}</span>}
+    <div className="container mx-auto p-5 bg-search-background bg-cover bg-center bg-no-repeat lg:py-28">
+      <h1 className="font-semibold text-2xl text-primaryDarker text-center lg:text-[2.5rem]">
+        Encontre sua próxima <span className="text-primary">viagem!</span>
+      </h1>
+
+      <div className="flex flex-col gap-4 mt-5 lg:flex-row lg:max-w-[948px] lg:mx-auto lg:p-4 lg:bg-primary lg:mt-12 lg:bg-opacity-20 lg:rounded-lg">
+        <Input
+          placeholder="Onde você quer ir?"
+          error={!!errors.text}
+          errorMessage={errors.text?.message}
+          {...register("text", {
+            required: {
+              value: true,
+              message: "Texto é obrigatório.",
+            },
+          })}
+        />
+
+        <div className="flex gap-4 lg:w-full">
+          <Controller
+            name="startDate"
+            control={control}
+            render={({ field }) => (
+              <DatePicker onChange={field.onChange} selected={field.value} placeholderText="Data Final" className="w-full" minDate={new Date()} />
+            )}
+          />
+
+          <Controller
+            name="budget"
+            control={control}
+            render={({ field }) => (
+              <CurrencyInput
+                allowDecimals={false}
+                placeholder="Orçamento"
+                onValueChange={field.onChange as any}
+                value={field.value}
+                onBlur={field.onBlur}
+              />
+            )}
+          />
+        </div>
+
+        <Button onClick={() => handleSubmit(onSubmit)()} className="w-1/2 lg:h-fit">
+          Buscar
+        </Button>
+      </div>
     </div>
   );
-}
+};
 
-export default forwardRef(Input);
+export default TripSearch;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import TripSearch from "./components/TripSearch";
+import QuickSearch from "./components/quickSearch";
 
 export default function Home() {
   return (
     <div>
       <TripSearch />
-
+      <QuickSearch />
     </div>
   );
 }
